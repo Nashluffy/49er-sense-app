dacffaeb649132ba964f46ff537715a9a82ab171
Nash Luffman <nashluffman@gmail.com>
2019-09-06 12:35:30 -0400

    First and second assignments

---
+++ src/IoT/ArrayString.java
@@ -0,0 +1,76 @@
+package IoT;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class ArrayString {
+	
+	public static boolean uniqueChar(String input) {
+		boolean[] flags = new boolean[128];
+		for (int i = 0; i < input.length(); i++) {
+			int asciiVal = (int) input.charAt(i);
+			if (flags[asciiVal]) {
+				return false;
+			}
+			else {
+				flags[asciiVal] = true;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean permutation(String one, String two) {
+		char[] charArrayOne = one.toCharArray();
+		char[] charArrayTwo = two.toCharArray();
+		Arrays.sort(charArrayOne);
+		Arrays.sort(charArrayTwo);
+		if (Arrays.equals(charArrayOne, charArrayTwo)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static boolean palindromePermutation(String one) {
+		int[] asciiArray = new int[128];
+		for (int i = 0; i < one.length(); i++) {
+			asciiArray[(int) one.charAt(i)] += 1;
+		}
+		if(one.length()%2 == 0) {
+			for(int i = 0; i < asciiArray.length; i++) {
+				if (asciiArray[i]%2 != 0) {
+					return false;
+				}
+			}
+		}
+		else {
+			boolean oddNumber = false;
+			for(int i = 0; i < asciiArray.length; i++) {
+				if (asciiArray[i]%2 != 0) {
+					if(oddNumber) {return false;}
+					else {oddNumber = true;}
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		
+		//Testing first problem, should return false then true
+		System.out.println("String is free of duplicates: " + uniqueChar("LotsOfDuplicates"));
+		System.out.println("String is free of duplicates: " + uniqueChar("NoDuplicates"));
+		
+		//Testing second problem, should return true then false
+		System.out.println("Strings are permutations: " + permutation("god","dog"));
+		System.out.println("Strings are permutations: " + permutation("Not","Perm"));
+		
+		//Testing third problem. First two test even, second test odd
+		System.out.println("Odd string is a permutation of a palindrome: " + palindromePermutation("opooo"));
+		System.out.println("Odd string is a permutation of a palindrome: " + palindromePermutation("oppoo"));
+		System.out.println("Even string is a permutation of a palindrome: " + palindromePermutation("aadd"));
+		System.out.println("Even string is a permutation of a palindrome: " + palindromePermutation("aada"));
+	}
+
+}
---
+++ src/IoT/LinkedList.java
@@ -0,0 +1,143 @@
+package IoT;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedList {
+
+	Node head;
+	
+	static class Node {
+		int data;
+		Node next;
+		Node(int d) {
+			data = d;
+			next = null;
+		}
+	}
+	
+	public void insert(int data) {
+		Node new_node = new Node(data);
+		new_node.next = null;
+		if(this.head == null) {
+			this.head = new_node;
+		}
+		else {
+			Node currentNode = this.head;
+			while (currentNode.next != null) {
+				currentNode = currentNode.next;
+			}
+			currentNode.next = new_node;
+		}
+	}
+	
+	public void sort() {
+		Node outerNode = this.head;
+		Node innerNode = this.head;
+		while(outerNode.next != null) {
+			while(innerNode.next != null) {
+				if(innerNode.next.data > outerNode.next.data) {
+					int temp = innerNode.next.data;
+					innerNode.next.data = outerNode.next.data;
+					outerNode.next.data = temp;
+				}
+				innerNode = innerNode.next;
+			}
+			innerNode = this.head;
+			outerNode = outerNode.next;
+		}
+	}
+	
+	
+	public void removeDuplicates() {
+		Node currentIterator = this.head;
+		Node previousIterator = null;
+		
+		Set<Integer> values = new HashSet<>();
+		
+		while(currentIterator != null) {
+			if (values.contains(currentIterator.data)) {
+				previousIterator.next = currentIterator.next;
+				values.remove(currentIterator);
+			}
+			else {
+				values.add(currentIterator.data);
+				previousIterator = currentIterator;
+			}
+			currentIterator = previousIterator.next;
+		}
+		
+	}
+	
+	public void print() {
+		Node iteratorNode = this.head;
+		while (iteratorNode != null) {
+			System.out.print(iteratorNode.data);
+			System.out.println();
+			iteratorNode = iteratorNode.next;
+		}
+	}
+	
+	public void palindrome() {
+		Node iteratorNode = this.head;
+		StringBuilder WIP = new StringBuilder("");
+		String reversed;
+		while(iteratorNode != null) {
+			WIP.append(iteratorNode.data);
+			
+			iteratorNode = iteratorNode.next;
+		}
+		reversed = WIP.reverse().toString();
+		WIP.reverse();
+		if(reversed.equals(WIP.toString())) {
+			System.out.print("The LinkedList is a Palindrome!");
+			System.out.println();
+			System.out.print("Reversed String: ");
+			System.out.print(WIP.reverse().toString());
+			System.out.println();
+			System.out.print("Final String: ");
+			System.out.print(WIP.toString());
+			System.out.println();
+		}
+		else {
+			System.out.print("The LinkedList is not a Palindrome");
+			System.out.println();
+			System.out.print("Reversed String: ");
+			System.out.print(WIP.reverse().toString());
+			System.out.println();
+			System.out.print("Final String: ");
+			System.out.print(WIP.toString());
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		
+		//Insert a Palindrome
+		list.insert(1);
+		list.insert(2);
+		list.insert(3);
+		list.insert(4);
+		list.insert(5);
+		list.insert(5);
+		list.insert(4);
+		list.insert(3);
+		list.insert(2);
+		list.insert(1);
+	
+		// Testing Palindrome
+		list.palindrome();
+		
+		
+		//Testing sort
+		list.sort();
+		list.print();
+		
+		//Testing duplicate removal
+		list.removeDuplicates();
+		list.print();
+	}
+	
+}
